3b197089cb9a0646c243869edadc3d6d4ab5da50
fawda123 <mbafs2012@gmail.com>
2026-01-02 08:30:54 -0500

    add 2025 data

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,8 +2,20 @@ library(tbeptools)
 
 noaa_key <- Sys.getenv('NOAA_KEY')
 
-yrs <- seq(2012, 2024)
+# complete, doesn't work anymore -----------------------------------------
+
+yrs <- seq(2012, 2025)
 
 yrests <- anlz_hydroload(yrs, noaa_key, trace = T)
 
 save(yrests, file = 'data/yrests.RData')
+
+# add current year to existing -------------------------------------------
+
+data(yrests)
+
+toadd <- anlz_hydroload(2025, noaa_key, trace = T)
+
+yrests <- dplyr::bind_rows(yrests, toadd)
+
+save(yrests, file = here::here('data/yrests.RData'))
